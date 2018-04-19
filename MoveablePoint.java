063a2bd9f3b6f49e2392fb09f3ee3a7ecde4c5b3
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-04-19 23:42:41 +0700

    pf-java-oop moveable point

---
+++ MoveablePoint.java
@@ -0,0 +1,59 @@
+public class MoveablePoint extends Point {
+    private float xSpeed;
+    private float ySpeed;
+
+    public MoveablePoint() {
+    }
+
+    public MoveablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MoveablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float getxSpeed() {
+        return xSpeed;
+    }
+
+    public void setxSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getySpeed() {
+        return ySpeed;
+    }
+
+    public void setySpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
+    public float[] getSpeed() {
+        return new float[]{xSpeed, ySpeed};
+    }
+
+    public void setSpeed(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MoveablePoint move() {
+        float x = getX() + getxSpeed();
+        float y = getY() + getySpeed();
+        setxSpeed(x);
+        setySpeed(y);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+"MoveablePoint{" +
+                "xSpeed=" + xSpeed +
+                ", ySpeed=" + ySpeed +
+                '}';
+    }
+}
---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        MoveablePoint moveablePoint = new MoveablePoint(5,6,7,8);
+        moveablePoint.move();
+        System.out.println(moveablePoint);
+    }
+}
